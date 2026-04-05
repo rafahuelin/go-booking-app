6ab14f41109ed52b9cbdba213783968424275eb8
rafahuelin <rafahuelin@gmail.com>
2026-04-05 15:19:36 +0000

    total tickets consntant and var

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = conferenceTickets
 
 	fmt.Println("Welcome to", conferenceName, "conference booking application.")
+	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend.")
 }
