b66f6182fa81e6e4e9c6b30a5764457fa51abc31
rafahuelin <rafahuelin@gmail.com>
2026-04-05 16:33:19 +0000

    print type

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = conferenceTickets
 
+	fmt.Printf("ConferenceName is %T, conferenceTickets is %T, remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
 	fmt.Printf("Welcome to %v conference booking application.\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
